fe4a6d9236f08b83f470074125cd0edecb2cc643
Andrew Laryea <laryeaandrew0001@gmail.com>
2025-02-23 22:38:08 +0000

    Trick

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import UserContext from "./Components/UserContext";
+import UserContext from "./UserContext";
 
 function UserDetails() {
   const userData = useContext(UserContext);
