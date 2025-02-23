836ce8d9420f5597f249765a0edcff4d2341ad63
Andrew Laryea <laryeaandrew0001@gmail.com>
2025-02-23 22:44:33 +0000

    tried

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
-import ProfilePage from "./ProfilePage";
-import UserContext from "./UserDetails";
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./components/UserDetails";
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
