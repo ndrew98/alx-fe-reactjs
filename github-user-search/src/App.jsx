6cce5409013ca7412df4ccf7815be0106bb63e52
Andrew Laryea <laryeaandrew0001@gmail.com>
2025-03-28 12:06:29 +0000

    working with APIs

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>GitHub User Search</h1>
+        </header>
+        <Search />
+      </div>
+    </>
+  );
+}
+
+export default App;
